a28a34901845c283822b79661c6bdc4c30ca86cb
glauberperez <glauber2070@hotmail.com>
2023-12-25 18:20:57 -0300

    getting somewhere

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Switch } from '@headlessui/react'
+import { Switch, Tab } from '@headlessui/react'
 
 
 
@@ -39,15 +39,28 @@ function App() {
                     } inline-block h-4 w-4 transform rounded-full bg-white transition`}
                   />
                 </Switch>
+
                 </div>
             </div>
         </nav>
     </header>
 
-    <div className="flex items-center justify-center w-screen h-screen">
+    <main className="py-5">
 
+      <div className="flex flex-wrap justify-center items-center mx-auto max-w-screen-xl backdrop-blur-3xl bg-gray-800">
+        <div className="flex flex-col justify-center items-center w-full px-4 lg:px-6 py-2.5">
+          <h1 className="text-3xl font-semibold text-center">Welcome to Diet Planner</h1>
+          <p className="text-center mt-1">This is a simple app that helps you to keep track of your diet and calories</p>
 
-    </div>
+          <h1 className="text-3xl font-semibold text-center mt-2">How it works?</h1>
+          <p className="text-center mt-1">You can add your meals and calories and the app will calculate the total calories and macros for you</p>
+
+          <h1 className="text-2xl font-semibold text-center m-5">Get started</h1>
+
+        </div>
+
+      </div>
+    </main>
 
     </>
   )

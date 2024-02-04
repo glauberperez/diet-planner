dfff52ae5113352280749f8291477d92833da6b5
glauberperez <glauber2070@hotmail.com>
2024-02-04 11:50:22 -0300

    little color fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = () => {
     return (
 
         <header>
-        <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+        <nav className="bg-gray-800 border-gray-200 px-4 lg:px-6 py-2.5">
             <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                 <a href="#" className="flex items-center">
                     <img src="appicon.svg" className="mr-3 h-6 sm:h-9" alt="dietplanner logo" />

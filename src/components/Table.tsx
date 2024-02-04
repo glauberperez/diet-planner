03f2f9e3e607f2b13cb558befff06385f5f5c634
glauberperez <glauber2070@hotmail.com>
2024-02-04 17:49:17 -0300

    table ok & fixing return

---
+++ src/components/Table.tsx
@@ -0,0 +1,91 @@
+import { useState, useEffect } from 'react'
+
+type Data = {
+    id: number;
+    name: string;
+    grams: string;
+    protein: string;
+    carb: string;
+    fat: string;
+    calories: string;
+  };
+
+function Table () {
+
+    const [data, setData] = useState([]);
+
+
+    fetch('https://run.mocky.io/v3/7a8d95ac-068f-46a0-abcb-18408ddb60af')
+    .then((res) => res.json())
+    .then((data) => {
+      setData(data);
+      console.log(typeof data);
+      console.log(data);
+    });
+
+    /*
+    useEffect(() => {
+        fetch('https://run.mocky.io/v3/557ef283-9e8e-40ff-bdad-5e707eb11c67')
+          .then((res) => res.json())
+          .then((data) => {
+            setData(data);
+            console.log(data);
+          });
+          //eslint-disable-next-line
+      }, []);
+      */
+
+    return (
+        <div className="flex flex-col">
+            <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+                <div className="overflow-hidden">
+                <table className="min-w-full text-left text-sm font-light">
+                    <thead className="border-b font-medium dark:border-neutral-500">
+                    <tr>
+                        <th scope="col" className="px-6 py-4">Name</th>
+                        <th scope="col" className="px-6 py-4">Grams</th>
+                        <th scope="col" className="px-6 py-4">Protein</th>
+                        <th scope="col" className="px-6 py-4">Carb</th>
+                        <th scope="col" className="px-6 py-4">Fat</th>
+                        <th scope="col" className="px-6 py-4">Calories</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr className="border-b transition duration-300 ease-in-out border-neutral-500 dark:hover:bg-neutral-600">
+                        <td className="whitespace-nowrap px-6 py-4 font-medium">Rice</td>
+                        <td className="whitespace-nowrap px-6 py-4">100 g</td>
+                        <td className="whitespace-nowrap px-6 py-4">20 g</td>
+                        <td className="whitespace-nowrap px-6 py-4">40 g</td>
+                        <td className="whitespace-nowrap px-6 py-4">40 g</td>
+                        <td className="whitespace-nowrap px-6 py-4">654 kcal</td>
+                    </tr>
+
+                    { /*
+
+                        data.map( (item: Data) => {
+                            return ( 
+                            <tr key={item.id} className="border-b transition duration-300 ease-in-out border-neutral-500 dark:hover:bg-neutral-600">
+                                <td className="whitespace-nowrap px-6 py-4 font-medium">{item.name}</td>
+                                <td className="whitespace-nowrap px-6 py-4">{item.grams}</td>
+                                <td className="whitespace-nowrap px-6 py-4">{item.protein}</td>
+                                <td className="whitespace-nowrap px-6 py-4">{item.carb}</td>
+                                <td className="whitespace-nowrap px-6 py-4">{item.fat}</td>
+                                <td className="whitespace-nowrap px-6 py-4">{item.calories}</td>
+                            </tr>
+                            )
+                        })
+
+                        */
+                    }
+
+                    </tbody>
+                </table>
+                </div>
+            </div>
+            </div>
+        </div>
+        )
+}
+
+export default Table
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,54 +1,10 @@
-import React, { useMemo } from 'react'
-import { useTable } from 'react-table'
 
+import { useState, useEffect } from 'react'
+
+import Table from '../components/Table'
 
 
 const Main = () => {
-
-    fetch("https://run.mocky.io/v3/1b883419-eb52-43a1-8ca4-9fb98a955c22")
-    .then(response => response.json())
-    .then(data => console.log(data))
-
-
-    const data = React.useMemo(
-        () => [
-          {
-            col1: 'Hello',
-            col2: 'World',
-          },
-          {
-            col1: 'react-table',
-            col2: 'rocks',
-          },
-          {
-            col1: 'whatever',
-            col2: 'you want',
-          },
-        ],
-        []
-      )
-    
-      const columns = React.useMemo(
-        () => [
-          {
-            Header: 'Column 1',
-            accessor: 'col1', // accessor is the "key" in the data
-          },
-          {
-            Header: 'Column 2',
-            accessor: 'col2',
-          },
-        ],
-        []
-      )
-    
-      const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,
-      } = useTable({ columns, data })
 
 
     return (
@@ -58,29 +14,7 @@ const Main = () => {
 
         <div className="flex flex-wrap justify-center items-center mx-auto max-w-screen-xl backdrop-blur-3xl bg-gray-800">
 
-        <table {...getTableProps()}>
-          <thead>
-            {headerGroups.map(headerGroup => (
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map(column => (
-                  <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody {...getTableBodyProps()}>
-            {rows.map(row => {
-              prepareRow(row)
-              return (
-                <tr {...row.getRowProps()}>
-                  {row.cells.map(cell => {
-                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                  })}
-                </tr>
-              )
-            })}
-          </tbody>
-        </table>
+            <Table />
 
         </div>
         
